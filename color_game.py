acd72896a3c1f01f09872451f4d538f36e5351d9
shikamarumind <72576008+shikamarumind@users.noreply.github.com>
2020-11-01 22:27:56 -0600

    Add files via upload
    DAOcolors final version SCORE

--- color_game.py
+++ color_game.py
@@ -1,7 +1,7 @@
 from iconservice import *
 
 TAG = 'Colors'
-DEBUG = True
+ICX = 1000000000000000000
 BET_MIN = 0.1
 SIDE_BET_TYPES = ["specific_double", "specific_triple", "any_triple"]
 SIDE_BET_MULTIPLIERS = [10, 180, 30]
@@ -26,15 +26,11 @@ class Colors(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        if DEBUG is True:
-            Logger.debug(f'In __init__.', TAG)
-            Logger.debug(f'owner is {self.owner}.', TAG)
         self._game_on = VarDB(self._GAME_ON, db, value_type=bool)
         self._roulette_score = VarDB(self._ROULETTE_SCORE, db, value_type=Address)
 
     def on_install(self) -> None:
         super().on_install()
-        self._game_on.set(False)
 
     def on_update(self) -> None:
         super().on_update()
@@ -43,90 +39,46 @@ class Colors(IconScoreBase):
     def RollsResult(self, results: str):
         pass
 
-    @eventlog(indexed=1)
-    def BetPayouts(self, main_bet_win: str):
+    @eventlog(indexed=2)
+    def MainBet(self, main_bet_win: str, main_bet_payout: str):
+        pass
+
+    @eventlog(indexed=2)
+    def SideBet(self, side_bet_win: str, side_bet_payout: str):
         pass
 
     @eventlog(indexed=1)
-    def SidePayouts(self, side_bet_win: str):
-        pass
-
-    @eventlog(indexed=1)
-    def PayoutAmount(self, payout: str):
+    def TotalPayoutAmount(self, payout: str):
         pass
 
     @eventlog(indexed=2)
     def FundTransfer(self, recipient: Address, amount: int, note: str):
         pass
 
-    @external(readonly=True)
-    def get_score_owner(self) -> Address:
-        """
-        A function to return the owner of this score.
-        :return: Owner address of this score
-        :rtype: :class:`iconservice.base.address.Address`
-        """
-        return self.owner
-
     @external
-    def set_roulette_score(self, _score: Address) -> None:
-        """
-        Sets the roulette score address. The function can only be invoked by score owner.
-        :param _score: Score address of the roulette
-        :type _score: :class:`iconservice.base.address.Address`
-        """
-        if self.msg.sender == self.owner:
-            self._roulette_score.set(_score)
+    def set_roulette_score(self, _scoreAddress: Address) -> None:
+        if self.msg.sender != self.owner:
+            revert('Only the owner can call the set_roulette_score method')
+        self._roulette_score.set(_scoreAddress)
 
     @external(readonly=True)
     def get_roulette_score(self) -> Address:
-        """
-        Returns the roulette score address.
-        :return: Address of the roulette score
-        :rtype: :class:`iconservice.base.address.Address`
-        """
         return self._roulette_score.get()
 
     @external
-    def game_on(self) -> None:
-        """
-        Set the status of game as on. Only the owner of the game can call this method. Owner must have set the
-        roulette score before changing the game status as on.
-        """
+    def toggle_game_status(self) -> None:
         if self.msg.sender != self.owner:
             revert('Only the owner can call the game_on method')
-        if not self._game_on.get() and self._roulette_score.get() is not None:
-            self._game_on.set(True)
-
-    @external
-    def game_off(self) -> None:
-        """
-        Set the status of game as off. Only the owner of the game can call this method.
-        """
-        if self.msg.sender != self.owner:
-            revert('Only the owner can call the game_on method')
-        if self._game_on.get():
-            self._game_on.set(False)
+        if self._roulette_score.get() is not None:
+            self._game_on.set(not self._game_on.get())
 
     @external(readonly=True)
-    def get_game_on(self) -> bool:
-        """
-        Returns the current game status
-        :return: Current game status
-        :rtype: bool
-        """
+    def get_game_status(self) -> bool:
         return self._game_on.get()
 
-    @external
-    def untether(self) -> None:
-        """
-        A function to redefine the value of  self.owner once it is possible .
-        To  be included through an update if it is added to ICONSERVICE
-        Sets the value of self.owner to the score holding the game treasury
-        """
-        if self.msg.sender != self.owner:
-            revert('Only the owner can call the untether method ')
-        pass
+    @external(readonly=True)
+    def get_score_owner(self) -> Address:
+        return self.owner
 
     def get_random(self, user_seed: str = '', rolls: int = 0) -> float:
         Logger.debug(f'Entered get_random.', TAG)
@@ -157,18 +109,18 @@ class Colors(IconScoreBase):
     def bet(self, yellow: str, white: str, pink: str, blue: str, red: str, green: str,
             s_yellow: str, s_white: str, s_pink: str, s_blue: str, s_red: str, s_green: str,
             side_bet_type: str = '', user_seed: str = '') -> None:
-        yellow = float(yellow)
-        white = float(white)
-        pink = float(pink)
-        blue = float(blue)
-        red = float(red)
-        green = float(green)
-        s_yellow = float(s_yellow)
-        s_white = float(s_white)
-        s_pink = float(s_pink)
-        s_blue = float(s_blue)
-        s_red = float(s_red)
-        s_green = float(s_green)
+        yellow = float(yellow) * ICX
+        white = float(white) * ICX
+        pink = float(pink) * ICX
+        blue = float(blue) * ICX
+        red = float(red) * ICX
+        green = float(green) * ICX
+        s_yellow = float(s_yellow) * ICX
+        s_white = float(s_white) * ICX
+        s_pink = float(s_pink) * ICX
+        s_blue = float(s_blue) * ICX
+        s_red = float(s_red) * ICX
+        s_green = float(s_green) * ICX
         main_bet_payout = 0
         side_bet_payout = 0
         payout = side_bet_payout + main_bet_payout
@@ -176,17 +128,14 @@ class Colors(IconScoreBase):
         side_bet_set = False
         roulette_score = self.create_interface_score(self._roulette_score.get(), RouletteInterface)
         _treasury_min = roulette_score.get_treasury_min()
-        self.icx.transfer(self._roulette_score.get(), self.msg.value)
-        self.FundTransfer(self._roulette_score.get(), self.msg.value, "Sending icx to Roulette")
-        roulette_score.take_wager(self.msg.value)
         if not self._game_on.get():
             Logger.debug(f'Game not active yet.', TAG)
             revert(f'Game not active yet.')
-        if not (0 <= yellow <= 100 and 0 <= white <= 100 and 0 <= pink <= 100 and 0 <= blue <= 100 and
-                0 <= red <= 100 and 0 <= green <= 100):
+        if not (0 <= yellow <= 100*ICX and 0 <= white <= 100*ICX and 0 <= pink <= 100*ICX and 0 <= blue <= 100*ICX and
+                0 <= red <= 100*ICX and 0 <= green <= 100*ICX):
             Logger.debug(f'Bets placed out of range numbers', TAG)
             revert(f'Invalid bet. Choose a number between 0 to 100')
-        if not (0 <= yellow + white + pink + blue + red + green <= 100):
+        if not (0 <= yellow + white + pink + blue + red + green <= 100*ICX):
             Logger.debug(f'Maximum bet exceeded', TAG)
             revert(f'Maximum bet exceeded. Your total bet should be between 0 and 100')
         side_bet_amount = s_yellow + s_white + s_pink + s_blue + s_red + s_green
@@ -200,7 +149,7 @@ class Colors(IconScoreBase):
             if side_bet_type not in SIDE_BET_TYPES:
                 Logger.debug(f'Invalid side bet type', TAG)
                 revert(f'Invalid side bet type.')
-            side_bet_limit = 10
+            side_bet_limit = 10*ICX
             if side_bet_amount < BET_MIN or side_bet_amount > side_bet_limit:
                 Logger.debug(f'Betting amount {side_bet_amount} out of range.', TAG)
                 revert(f'Betting amount {side_bet_amount} out of range ({BET_MIN} ,{side_bet_limit}).')
@@ -208,10 +157,18 @@ class Colors(IconScoreBase):
         if main_bet_amount == 0:
             Logger.debug(f'No main bet amount provided', TAG)
             revert(f'No main bet amount provided')
-        main_bet_limit = 100
+        main_bet_limit = 100*ICX
         if main_bet_amount < BET_MIN or main_bet_amount > main_bet_limit:
             Logger.debug(f'Betting amount {main_bet_amount} out of range.', TAG)
             revert(f'Main Bet amount {main_bet_amount} out of range {BET_MIN},{main_bet_limit} ')
+        if not (yellow + white + pink + blue + red + green) == (self.msg.value - side_bet_amount):
+            Logger.debug(f'Invalid bet. Main bet value must equal Main bet amount', TAG)
+            revert(f'Main Bet amount {main_bet_amount} doesnt equal Main bet Value {self.msg.value - side_bet_amount} ')
+        if not (s_yellow + s_white + s_pink + s_blue + s_red + s_green) == (self.msg.value - main_bet_amount):
+            Logger.debug(f'Invalid bet. Side bet value must equal Side bet amount', TAG)
+            revert(f'Side Bet amount {side_bet_amount} doesnt equal Side bet Value {self.msg.value - main_bet_amount} ')
+        total_bet_amount = int(main_bet_amount + side_bet_amount)
+
 
         # execute rolls
         colors = ['Y', 'W', 'P', 'B', 'R', 'G']
@@ -252,22 +209,28 @@ class Colors(IconScoreBase):
         if side_bet_set:
             if side_bet_type == SIDE_BET_TYPES[0]:  # specific double
                 for m in user_side:
-                    color_count = 0
-                    for k in results:
-                        if m == k:
-                            color_count += 1
-                    if color_count == 2 or color_count == 3:
-                        s_payout += user_side[m] + (user_side[m] * 10)
-                        side_bet_win = True
+                    if user_side[m] == 0:
+                        continue
+                    else:
+                        color_count = 0
+                        for k in results:
+                            if m == k:
+                                color_count += 1
+                        if color_count == 2 or color_count == 3:
+                            s_payout += user_side[m] + (user_side[m] * 10)
+                            side_bet_win = True
             elif side_bet_type == SIDE_BET_TYPES[1]:  # specific triple
                 for n in user_side:
-                    color_count = 0
-                    for k in results:
-                        if n == k:
-                            color_count += 1
-                    if color_count == 3:
-                        s_payout += user_side[n] + (user_side[n] * 180)
-                        side_bet_win = True
+                    if user_side[n] == 0:
+                        continue
+                    else:
+                        color_count = 0
+                        for k in results:
+                            if n == k:
+                                color_count += 1
+                        if color_count == 3:
+                            s_payout += user_side[n] + (user_side[n] * 180)
+                            side_bet_win = True
             elif side_bet_type == SIDE_BET_TYPES[2]:  # any triple
                 if results[0] == results[1] == results[2]:
                     total_user_side_bet_amount = 0
@@ -281,21 +244,27 @@ class Colors(IconScoreBase):
         # payout
         main_bet_payout += m_payout
         side_bet_payout += s_payout
-        payout = main_bet_payout + side_bet_payout
-        self.PayoutAmount(str(payout))
-        self.BetPayouts(str(main_bet_win))
-        self.SidePayouts(str(side_bet_win))
-        if main_bet_win or side_bet_win:
-            Logger.debug(f'Amount owed to winner: {payout}', TAG)
-            try:
-                Logger.debug(f'Trying to send to ({self.tx.origin}): {payout}.', TAG)
-                roulette_score.wager_payout(payout)
-                Logger.debug(f'Sent winner ({self.tx.origin}) {payout}.', TAG)
-            except BaseException as e:
-                Logger.debug(f'Send failed. Exception: {e}', TAG)
-                revert('Network problem. Winnings not sent. Returning funds.')
+        payout = int(main_bet_payout + side_bet_payout)
+        if payout == 0:
+            self._take_wager(total_bet_amount)
         else:
-            Logger.debug(f'Player lost. ICX retained in treasury.', TAG)
+            self._take_wager_and_request_payout(total_bet_amount, payout)
+        self.TotalPayoutAmount(str(payout))
+        self.MainBet(str(main_bet_win), str(main_bet_payout))
+        self.SideBet(str(side_bet_win), str(side_bet_payout))
+
+    def _take_wager(self, _wager: int):
+        self.icx.transfer(self._roulette_score.get(), _wager)
+        self.FundTransfer(self._roulette_score.get(), _wager, "Sending icx to roulette")
+        roulette_score = self.create_interface_score(self._roulette_score.get(),RouletteInterface)
+        roulette_score.take_wager(_wager)
+
+    def _take_wager_and_request_payout(self, _wager: int, _payout: int):
+        self.icx.transfer(self._roulette_score.get(), _wager)
+        self.FundTransfer(self._roulette_score.get(), _wager, "Sending icx to roulette")
+        roulette_score = self.create_interface_score(self._roulette_score.get(),RouletteInterface)
+        roulette_score.take_wager(_wager)
+        roulette_score.wager_payout(_payout)
 
     @payable
     def fallback(self):
